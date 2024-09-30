95826227e652428059fdfd407b86f1dc211bee6b
PoliEcho <poliecho@pupes.org>
2024-09-30 16:41:51 +0200

    here comes nothing

--- app/src/main/java/com/nechcemeobojek/hackathonarcha/MainActivity.kt
+++ app/src/main/java/com/nechcemeobojek/hackathonarcha/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.nechcemeobojek.hackathonarcha
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.nechcemeobojek.hackathonarcha.ui.theme.HackathonArchaTheme
 
@@ -19,29 +28,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HackathonArchaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Slot_machine(
+                        modifier = Modifier,
+                        username = "testu"
                     )
+
+                }
                 }
             }
         }
     }
-}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Slot_machine(username: String, modifier: Modifier = Modifier) {
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+                    .fillMaxSize()
+        ) {
+            Text(
+                text = "Double or nothing"
+            )
+            Button(onClick = {  }) {
+                Text(stringResource(R.string.Coin_flip_button_text))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HackathonArchaTheme {
-        Greeting("Android")
+        Slot_machine("testuser")
     }
 }
